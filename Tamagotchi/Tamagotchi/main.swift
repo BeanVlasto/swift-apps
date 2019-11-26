800659d3c7153a5ff5848cfc2ac4c5b863587748
BeanVlasto <Vlasto.B@etoncollege.org.uk>
2019-11-26 10:21:18 +0000

    Initial empty project

---
+++ Tamagotchi/Tamagotchi/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Tamagotchi
+//
+//  Created by Vlasto, Benedict (JDN) on 26/11/2019.
+//  Copyright © 2019 Vlasto, Benedict (JDN). All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
