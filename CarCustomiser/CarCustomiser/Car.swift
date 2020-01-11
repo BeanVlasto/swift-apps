febf641b0094342e9400420f965746a8bd83d370
BeanVlasto <Vlasto.B@etoncollege.org.uk>
2020-01-11 19:06:59 +0000

    Introductory car customiser files

---
+++ CarCustomiser/CarCustomiser/Car.swift
@@ -0,0 +1,28 @@
+//
+//  Car.swift
+//  CarCustomiser
+//
+//  Created by Vlasto, Benedict (JDN) on 11/01/2020.
+//  Copyright © 2020 Vlasto, Benedict (JDN). All rights reserved.
+//
+
+import Foundation
+
+struct Car {
+    
+    var make: String
+    var model: String
+    var topSpeed: Int
+    var acceleration: Double
+    var handling: Int
+    
+    func displayStats() -> String {
+        return """
+        Make: \(make)
+        Model: \(model)
+        Top speed: \(topSpeed)
+        Acceleration: \(acceleration)
+        Handling: \(handling)
+        """
+    }
+}
---
+++ CarCustomiser/CarCustomiser/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CarCustomiser
+//
+//  Created by Vlasto, Benedict (JDN) on 11/01/2020.
+//  Copyright © 2020 Vlasto, Benedict (JDN). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var car = Car(make: "BMW", model: "i8", topSpeed: 200, acceleration: 4.3, handling: 7)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(car.model)
+    }
+
+
+}
+
