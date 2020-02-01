24721839ac09ae2aa480ed154fe48d9711ee6159
BeanVlasto <Vlasto.B@etoncollege.org.uk>
2020-02-01 17:52:16 +0000

    finished basic navigation menu including students for each division being shown

---
+++ AbsenceRecorder/AbsenceRecorder/DivisionAbsenceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DivisionAbsenceViewController.swift
+//  AbsenceRecorder
+//
+//  Created by Vlasto, Benedict (JDN) on 01/02/2020.
+//  Copyright © 2020 Vlasto, Benedict (JDN). All rights reserved.
+//
+
+import UIKit
+
+class DivisionAbsenceViewController: UITableViewController {
+    
+    var division: Division?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return division?.students.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Student", for: indexPath)
+        cell.textLabel?.text = division?.students[indexPath.row].forename
+        return cell
+    }
+
+}
--- AbsenceRecorder/AbsenceRecorder/ViewController.swift
+++ AbsenceRecorder/AbsenceRecorder/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UITableViewController {
     
     var divisions: [Division] = []
-
+    var currentDate: Date = Date()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -22,6 +23,8 @@ class ViewController: UITableViewController {
                 print(eachStudent.forename)
             }
         }
+        
+        updateDateDisplay()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -30,16 +33,40 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Division", for: indexPath)
-        
         cell.textLabel?.text = divisions[indexPath.row].code
-        
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let vc  = storyboard?.instantiateViewController(withIdentifier: "DivisionAbsenceViewController") as? DivisionAbsenceViewController else {
+            fatalError("Failed to load Division Absence View Controller from Storyboard.")
+        }
+        
+        vc.division = divisions[indexPath.row]
+        
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func updateDateDisplay() {
+           let formatter = DateFormatter()
+           formatter.dateStyle = .medium
+           navigationItem.title = formatter.string(from: currentDate)
     }
 
     func addDummyData() {
         divisions.append(DivisionFactory.createDivision(code: "YEET", size: 10))
         divisions.append(DivisionFactory.createDivision(code: "BrEh", size: 6))
     }
-
+    
+    @IBAction func previousDay(_ sender: Any) {
+        currentDate = Calendar.current.date(byAdding: .day, value: -1, to: currentDate) ?? Date()
+        updateDateDisplay()
+    }
+    
+    @IBAction func nextDay(_ sender: Any) {
+        currentDate = Calendar.current.date(byAdding: .day, value: 1, to: currentDate) ?? Date()
+        updateDateDisplay()
+    }
+    
 }
 
