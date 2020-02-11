6876890e46f02f7b9f17182861de17b2f970b86d
BeanVlasto <Vlasto.B@etoncollege.org.uk>
2020-02-11 10:51:10 +0000

    Adding new version of CommentCardWriter

---
+++ CommentCardWriter/CommentCardWriter/CommentCard.swift
@@ -0,0 +1,9 @@
+//
+//  CommentCard.swift
+//  CommentCardWriter
+//
+//  Created by Vlasto, Benedict (JDN) on 11/02/2020.
+//  Copyright © 2020 Vlasto, Benedict (JDN). All rights reserved.
+//
+
+import Foundation
