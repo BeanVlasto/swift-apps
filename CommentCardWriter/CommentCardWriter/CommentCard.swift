44ecc15370143cebfbe8ad0a9eb0328c6b4544a1
BeanVlasto <Vlasto.B@etoncollege.org.uk>
2020-02-11 11:23:20 +0000

    creating display with pickers

--- CommentCardWriter/CommentCardWriter/CommentCard.swift
+++ CommentCardWriter/CommentCardWriter/CommentCard.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+class CommentCard {
+    
+    let subject: String
+    //let subjectCode: String
+    let teacher: String
+    var content: String
+    
+    init(subject: String, teacher: String, content: String) {
+        self.subject = subject
+        self.teacher = teacher
+        self.content = content
+    }
+    
+    /*func returnCommentCardContent() -> String {
+        let commentCardWriter = CommentCardWriter()
+        return commentCardWriter.writeContent(strength: "EWs", weakness: "the classwork", thingToImprove: "asking questions in class.")
+    }*/
+    
+}
--- CommentCardWriter/CommentCardWriter/ViewController.swift
+++ CommentCardWriter/CommentCardWriter/ViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    
+    
+    @IBOutlet var strengthPicker: UIPickerView!
+    @IBOutlet var weaknessPicker: UIPickerView!
+    @IBOutlet var improvementPicker: UIPickerView!
+    
+    let commentCard = CommentCard(subject: "computer science", teacher: "DPC", content: "")
+    let commentCardWriter = CommentCardWriter()
+    let strengthArray = ["EW", "Thing", "bread"]
+    let weaknessArray = ["classwork"]
+    let improvementArray = ["asking questions"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        createCommentCardContent()
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == strengthPicker {
+            return strengthArray.count
+        } else if pickerView == weaknessPicker {
+            return weaknessArray.count
+        } else {
+            return improvementArray.count
+        }
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func createCommentCardContent() {
+        commentCard.content = commentCardWriter.writeContent(strength: "EWs", weakness: "the classwork", thingToImprove: "ask more questions in class")
+        print(commentCard.content)
     }
 
-
 }
-
